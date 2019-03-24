ef5f26424a13042bd2ade7c17237b8ee945ad377
Monica Mendes <monicamendesmontanha@gmail.com>
2019-03-25 10:52:29 +1100

    more of symbols

--- 02-html_css/capabilities/js/capabilities.js
+++ 02-html_css/capabilities/js/capabilities.js
@@ -1,11 +1,11 @@
 console.log("%c---HTML & CSS: capabilities---", "color: blue; background-color: yellow;");
 
 //start with an empty string
-
+// extended  ASCCI
 let content = '';
 
 // loop throungh the available characteres
-for (let i = 0; i < 227; i++){
+for (let i = 0; i < 2 ** 14; i++){
   content += String.fromCharCode(i)
 }
 
