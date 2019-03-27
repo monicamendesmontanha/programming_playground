0beed1fdd68fed54ec6ba6ea444b95d7439e246d
Monica Mendes <monicamendesmontanha@gmail.com>
2019-03-27 12:58:20 +1100

    the book list - manipulation the dom

--- 02-html_css/bookList/bookList.js
+++ 02-html_css/bookList/bookList.js
@@ -1,5 +1,14 @@
 console.log("%c---My Book List---", "color: blue; font-size: large; background-color: yellow;");
 
+// Keep track of which books you read and which books you want to read!
+
+// Create a webpage with an h1 of "My Book List".
+// Create a new Javascript file and link to it with a script tag at the bottom.
+// Copy this array of books:
+// Iterate through the array of books. For each book, create a p element with the book title and author and append it to the page.
+// X Bonus: Use a ul and li to display the books.
+// X Bonus: add a property to each book with the URL of the book cover, and add an img element for each book on the page.
+// Bonus: Change the style of the book depending on whether you have read it or not.
 
 var books = [
   {title: 'The Design of EveryDay Things',
@@ -10,3 +19,22 @@ var books = [
   author: 'Brian Christian',
   alreadyRead: true
   }];
+
+  //Iterate through the array of books. For each book, create a p element with the book title and author and append it to the page.
+  const ul = document.createElement('ul');
+
+  for (let i = 0 ; i < books.length ; i++) {
+    const book = books[i];
+    const p = document.createElement('p');
+    const li = document.createElement('li');
+    p.innerHTML = `${book.title} by ${book.author}.`;
+    if(book.alreadyRead === true) {
+      li.style.opacity = 0.4;
+    }
+    document.body.appendChild(p);
+    ul.appendChild(li);
+  }
+
+  //document.body.appendChild(ul);
+
+  //console.log(ul);
