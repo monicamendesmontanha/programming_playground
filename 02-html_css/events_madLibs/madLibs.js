834e1b00bf0bf9d38c9afbc422516556a1ee8dfc
Monica Mendes <monicamendesmontanha@gmail.com>
2019-03-27 14:14:37 +1100

    events: mad libs exercise

---
+++ 02-html_css/events_madLibs/madLibs.js
@@ -0,0 +1,9 @@
+console.log("%c---Events: Mad Libs---", "color: blue; font-size: large; background-color: yellow;");
+
+// Exercises: Events
+// Mad Libs
+// Start with this webpage, which has several input elements and a button:
+// Create a new Javascript file and link to it with a script tag at the bottom.
+// Add an event listener to the button so that it calls a makeMadLib function when clicked.
+// In the makeMadLib function, retrieve the current values of the form input elements, make a story from them, and output that in the story div (like "Pamela really likes pink cucumbers.")
+
