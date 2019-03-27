34fdefb78ed19e934050bac8a214209de32f6bc5
Monica Mendes <monicamendesmontanha@gmail.com>
2019-03-27 12:38:57 +1100

    The Book List Exercise

---
+++ 02-html_css/bookList/bookList.js
@@ -0,0 +1,12 @@
+console.log("%c---My Book List---", "color: blue; font-size: large; background-color: yellow;");
+
+
+var books = [
+  {title: 'The Design of EveryDay Things',
+   author: 'Don Norman',
+   alreadyRead: false
+  },
+  {title: 'The Most Human Human',
+  author: 'Brian Christian',
+  alreadyRead: true
+  }];
