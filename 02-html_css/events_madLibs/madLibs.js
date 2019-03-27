e250fd53d8e61aad84f6c799281192113f82467d
Monica Mendes <monicamendesmontanha@gmail.com>
2019-03-27 14:34:50 +1100

    solution of mad libs

--- 02-html_css/events_madLibs/madLibs.js
+++ 02-html_css/events_madLibs/madLibs.js
@@ -5,5 +5,24 @@ console.log("%c---Events: Mad Libs---", "color: blue; font-size: large; backgrou
 // Start with this webpage, which has several input elements and a button:
 // Create a new Javascript file and link to it with a script tag at the bottom.
 // Add an event listener to the button so that it calls a makeMadLib function when clicked.
-// In the makeMadLib function, retrieve the current values of the form input elements, make a story from them, and output that in the story div (like "Pamela really likes pink cucumbers.")
+// In the makeMadLib function, retrieve the current values of the form input elements,
+// make a story from them, and output that in the story div (like "Pamela really likes pink cucumbers.")
+
+const button = document.getElementById('lib-button');
+
+const makeMadLib = function() {
+
+  const noun = document.getElementById('noun').value;
+  const person = document.getElementById('person').value;
+  const adjective = document.getElementById('adjective').value;
+
+  alert(`${person} really likes ${adjective} ${noun}`);
+
+  console.log('fui clicado');
+  console.log(`${person} really likes ${adjective} ${noun}`);
+
+};
+
+button.addEventListener('click', makeMadLib);
+
 
