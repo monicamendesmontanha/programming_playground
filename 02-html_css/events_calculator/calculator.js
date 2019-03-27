50afa2c0d72c63884cfa8f0bdbe4e33a3d2f85cc
Monica Mendes <monicamendesmontanha@gmail.com>
2019-03-27 14:14:21 +1100

    events: calculator exercise

---
+++ 02-html_css/events_calculator/calculator.js
@@ -0,0 +1,9 @@
+console.log("%c---Events: Calculator---", "color: blue; font-size: large; background-color: yellow;");
+
+// Calculator
+// Start with this webpage, which has inputs and buttons for things to calculate:
+
+// Create a new Javascript file and link to it with a script tag at the bottom.
+// For each operation, create an event listener for the button, and when it's clicked, find the value of the appropriate input and show the result of the calculation in the solution div.
+// Bonus: respond to key presses so that the user doesn't have to click the button.
+
