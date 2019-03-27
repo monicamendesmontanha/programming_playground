e8dfe041bff0374307f8ea6097e84b5b01f730ac
Monica Mendes <monicamendesmontanha@gmail.com>
2019-03-27 14:44:58 +1100

    square number's solution

--- 02-html_css/events_calculator/calculator.js
+++ 02-html_css/events_calculator/calculator.js
@@ -4,6 +4,25 @@ console.log("%c---Events: Calculator---", "color: blue; font-size: large; backgr
 // Start with this webpage, which has inputs and buttons for things to calculate:
 
 // Create a new Javascript file and link to it with a script tag at the bottom.
-// For each operation, create an event listener for the button, and when it's clicked, find the value of the appropriate input and show the result of the calculation in the solution div.
+// For each operation, create an event listener for the button, and when it's clicked, find the
+// value of the appropriate input and show the result of the calculation in the solution div.
 // Bonus: respond to key presses so that the user doesn't have to click the button.
 
+// select the button
+const squareButton = document.getElementById('square-button');
+
+// define the event handler AKA event listeners AKA callbacks
+const squareNumber = function(){
+  // find the value
+  const input = Number(document.getElementById('square-input').value);
+  const result = input * input;
+  document.getElementById('solution').innerHTML = result;
+
+
+  // square the value
+  // display the result
+};
+
+squareButton.addEventListener('click', squareNumber);
+
+//attach the event handler to the button
