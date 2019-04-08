46867a5a3ce734659426ff35b05caf759831f501
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-08 14:31:36 +1000

    Add new method as a sum of numbers

--- 04-ruby/methods.rb
+++ 04-ruby/methods.rb
@@ -4,7 +4,14 @@ def hello(name="World", adjetive="Cruel") # Variadic
   puts "Hello #{adjetive} #{name}"
 end
 
-binding.pry # Equivalent to JS: debugger;
+def add(a,b)
+  result = a + b
+  return  # Implicit return: Ruby returns the value of the last line of code. 
+end
+
+puts "The result is #{ add 5, 7 }"
+
+# binding.pry # Equivalent to JS: debugger;
 
 hello
 hello()
