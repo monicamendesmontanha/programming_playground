2a5bfee932b1b789366734620d5ac4012675deea
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-08 13:01:18 +1000

    Solution of guess the number

---
+++ 04-ruby/loops/guess_the_number.rb
@@ -0,0 +1,33 @@
+# Title: Guess The Number
+# Activity:
+# You are to generate a basic "guess my number" game. The computer will pick a random number between 0 and 10. The user will guess the number until they guess correctly.
+# Specification:
+# The user should be asked to guess a number
+# If the user's guess is correct, the user should see a congratulatory message
+# If the user's guess is not correct, the user should be asked to guess the number again.
+# Extensions:
+# Let the user choose the maximum value (so they can play a long game with a random value between 0 and 10000, for example).
+# Give feedback to the user: "Wrong, guess higher!" or "Wrong, guess lower!"
+
+number = (rand() * 1000).to_i
+p number
+
+"Guess the number: "
+guess = 0
+
+until guess == number do
+
+  print "Guess the number: "
+  guess = gets.to_i
+
+  if guess < number
+    p "Wrong, guess higher!"
+  elsif guess > number
+    p "Wrong, guess lower!"
+  else
+    p "yeah! The correct number is #{number}!"
+  end
+
+end
+
+
