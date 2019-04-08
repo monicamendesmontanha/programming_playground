0a89645300054757badb7a322d183db70a08c321
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-08 11:19:12 +1000

    using print vs puts and gets.chomp

--- 04-ruby/greetings.rb
+++ 04-ruby/greetings.rb
@@ -4,3 +4,13 @@ print "What is yout name? :"
 name = gets.chomp
 
 puts "Your name is #{name}."
+
+print "Whats is yout surname? :"
+surname = gets.chomp
+
+puts "Your full name is #{name} #{surname}."
+
+print "What is your adress? :"
+adress = gets.chomp
+
+puts "Your name is #{name} #{surname} and you live at #{adress}."
