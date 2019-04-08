0615cb74f89d7f3f43cd3f798b2a1115b86e52a2
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-08 12:20:19 +1000

    Solution with if statement

---
+++ 04-ruby/conditionals/sydney_suburbs.rb
@@ -0,0 +1,17 @@
+# 3. Sydney Suburbs
+# Create a program that asks what suburbs you live in.
+# Depending on the answer, print an appropriate response of your choosing
+
+print "What's surbub do you live in: "
+residence = gets.chomp.capitalize
+
+if statement
+if residence == "Glebe"
+  p "Beautiful forehore walk"
+elsif residence == "Five Dock"
+  p "Cool gangland shooting"
+elsif residence == "Fairlight"
+  p "What a fancy name"
+else
+  p "I'm sure that's a lovely place"
+end
