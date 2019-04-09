e197632f0ef6fed482e0bc5dda7329633ff4cd46
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-09 12:43:52 +1000

    Refactor permutation using shuffle

--- 04-ruby/arrays/permutation_poems.rb
+++ 04-ruby/arrays/permutation_poems.rb
@@ -6,8 +6,15 @@ require 'pry'
 
 # Escrever qualquer texto que queria dps de chamar o arquivo na linha de comando e verá o mesmo resultado.
 # ruby permutation_poems.rb I AM THAT I AM
-words = ARGV
 
-words.permutation.each do |line|
-  puts line.join ' '
+words = %w{ I AM THAT I AM } #Default
+words = ARGV unless ARGV.empty?
+
+
+puts "-=" * 40
+puts words.join(' ').upcase.center 80
+puts "-=" * 40
+
+words.permutation.to_a.shuffle.each do |line|
+  puts line.join(' ').center 80
 end
