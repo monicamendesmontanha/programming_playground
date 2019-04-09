ae4c72aed1b31cfba5b31113082d5131daf1a78e
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-09 14:38:02 +1000

    Hashes - Learn how to use symbols

---
+++ 04-ruby/hash/symbols.rb
@@ -0,0 +1,19 @@
+# HASHES
+# digression into symbols
+# ruby keep tracking
+
+#---------------------
+# pry
+
+# true.object_id
+# => 20
+
+# false.object_id
+# => 0
+
+# 1.object_id
+# => 3
+
+
+# ruby don't recognize the string twice as a same object.
+# each typing, even when is the same string, return a object_id diferent.
