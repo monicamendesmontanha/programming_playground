82b4b55982eba466489bafeeab4355d0ab0b0bdb
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-09 12:40:31 +1000

    Using ARGV

--- 04-ruby/arrays/permutation_poems.rb
+++ 04-ruby/arrays/permutation_poems.rb
@@ -1,8 +1,13 @@
-# FIRST EXAMPLE
-words = %w{ I AM THAT I AM }
-p words
+require 'pry'
+# binding.pry
+
+# words = %w{ I AM THAT I AM }
+# p words
+
+# Escrever qualquer texto que queria dps de chamar o arquivo na linha de comando e verá o mesmo resultado.
+# ruby permutation_poems.rb I AM THAT I AM
+words = ARGV
 
 words.permutation.each do |line|
   puts line.join ' '
 end
-
