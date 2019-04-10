2017146aabed2aa1d7da775217aaeff6c1ced691
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-10 11:57:09 +1000

    Start calculator exercise using server

---
+++ 04-ruby/server/calculator/main.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'pry'
+
+# We can enter the port that we want (has to be more than 1234): ruby main.rb -p 8080
+
+get '/' do
+  erb :home
+end
+
+
+get '/faq' do
+  erb :faq
+end
