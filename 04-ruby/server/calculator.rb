e3817e659e436ec66a3278687b6d8bb3a3b00f45
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-10 11:40:59 +1000

    Discover a new significant problems

--- 04-ruby/server/calculator.rb
+++ 04-ruby/server/calculator.rb
@@ -5,3 +5,9 @@ get '/multiply/:x/:y' do
   result = params[:x].to_f * params[:y].to_f
   "The result is #{ result }."
 end
+
+# Significant problems:
+# 1. How do we user input without making them change the URL?
+# 2. Where is the HTML ?
+
+# Solution: Learn the convention (rules) to follow and know the configuration!!
