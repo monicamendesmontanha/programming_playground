4e2b880cfb4c093b992441d3f556b3e0bdc0b96c
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-10 10:48:26 +1000

    Intro to server

--- 04-ruby/server/intro_server.rb
+++ 04-ruby/server/intro_server.rb
@@ -1,3 +1,21 @@
-#SINATRA is a DSL for quickly creating web applications in Ruby with minimal effort.
+# SINATRA is a DSL for quickly creating web applications in Ruby with minimal effort.
+# DSL: Domain Specific Language
+# 127.0.0.1 is the number of my own computer
+# port number: :4567
+# call the /hello
+
+# Terminal: $ ruby intro_server.rb
+# open on the browser and typing the adress:
+# 127.0.0.1:4567/hello
+# 127.0.0.1:4567= localhost
+
 
 require 'sinatra'
+
+get '/hello' do
+  "Hello world from Sinatra!"
+end
+
+get '/lucky_number' do
+  "Here is your lucky number #{ rand 1..40 }."
+end
