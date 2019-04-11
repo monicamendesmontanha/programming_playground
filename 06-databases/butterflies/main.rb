682ee2b40deee97bf7daa74ceb1bdd205ed3a0f1
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-11 12:40:35 +1000

    Refactor the database and close the conection

--- 06-databases/butterflies/main.rb
+++ 06-databases/butterflies/main.rb
@@ -6,24 +6,23 @@ get '/' do
   erb :home
 end
 
-
 # INDEX - Show all butterflies
 get '/butterflies' do
-
-  db = SQLite3::Database.new 'database.sqlite3'
-  db.results_as_hash = true
-  @butterflies = db.execute 'SELECT * FROM butterflies'
-
+  @butterflies = query_db 'SELECT * FROM butterflies'
   erb :butterflies_index
 end
 
 # SHOW = Shows a single butterfly in more detail
 get '/butterflies/:id' do
+  butterflies = query_db "SELECT * FROM butterflies WHERE id=#{ params[:id] }"
+  @butterfly = butterflies.first
+  erb :butterflies_show
+end
 
+def query_db(sql_statement)
   db = SQLite3::Database.new 'database.sqlite3'
   db.results_as_hash = true
-  butterflies = db.execute "SELECT * FROM butterflies WHERE id=#{ params[:id] }"
-  @butterfly = butterflies.first
-
-  erb :butterflies_show
+  result = db.execute sql_statement
+  db.close  #close the conection
+  result # Implicity returned
 end
