9e27d6a78dda889d80a95feaa13b3d951a3155c3
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-11 11:10:28 +1000

    Intro about SQL database (sqlite3)

---
+++ 06-databases/intro_databases/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO people (id, first_name, last_name, age) VALUES (1, "John", "Smith", 64);
+INSERT INTO people (id, first_name, last_name, age) VALUES (2, "Jane", "Smith", 68);
+INSERT INTO people VALUES (3, "Bob", "Jones", 11);
