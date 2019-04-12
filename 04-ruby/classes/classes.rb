56ad783daef14de7d0e8ff8bee24380ced16f5d2
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-12 11:58:03 +1000

    Create the method initialize

--- 04-ruby/classes/classes.rb
+++ 04-ruby/classes/classes.rb
@@ -5,19 +5,26 @@ class MarxBrother
   # macro to write getters an setters for me
   attr_accessor :name, :instrument, :vice
 
-  def play
-    "My name is #{ @name } and I play the #{ @ instrument }"
+  def initialize(n, i, v)
+    @name = n
+    @instrument = i
+    @vice = v
+  end
+
+
+  def perform
+    "My name is #{ @name } and I play the #{ @instrument }"
   end
 end
 
-groucho = MarxBrother.new
-groucho.name= 'Groucho Marx'
-groucho.instrument= 'guitar'
-groucho.vice= 'cigars'
+groucho = MarxBrother.new 'Groucho Marx', 'guitar', 'cigars'
+# groucho.name = 'Groucho Marx'
+# groucho.instrument = 'guitar'
+# groucho.vice = 'cigars'
 
-harpo = MarxBrother.new
-harpo.name= 'Harpo Marx'
-harpo.instrument= 'harp'
+# harpo = MarxBrother.new
+# harpo.name = 'Harpo Marx'
+# harpo.instrument = 'harp'
 
 
 binding.pry
