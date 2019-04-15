bf3c93262cd79d87015e6b6013102059fa66bb55
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-15 11:36:40 +1000

    Fix the root route

--- 07-rails/basics/config/routes.rb
+++ 07-rails/basics/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root :to '/' => 'pages#hello'  # or get'/' => 'pages#hello'
+  root :to => 'pages#hello'  # or get'/' => 'pages#hello'
   get '/hello' => 'pages#hello'
   get '/faq' => 'pages#faq'
   get '/about' => 'pages#about'
+
+  get '/auto/:color' => 'auto#color'
+  get '/auto/:hp/:torque' => 'auto#engine'
 end
