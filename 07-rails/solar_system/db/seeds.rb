a706565b5213c20f480b14b873678e6e609ca1d1
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-16 10:55:39 +1000

    Fix the seeds.rb

--- 07-rails/solar_system/db/seeds.rb
+++ 07-rails/solar_system/db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Planet.destroy_all
+
+Planet.create :name => 'Earth', :obit => 24, :moons => 1
+Planet.create :name => 'Venus', :obit => 17, :moons => 0
+Planet.create :name => 'Mars', :obit => 33, :moons => 4
