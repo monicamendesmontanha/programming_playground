fb37b828336dacb50bd006690790e510c3aae663
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-23 14:10:16 +1000

    Tunr project

--- 07-rails/tunr/config/routes.rb
+++ 07-rails/tunr/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  resources :users, :only => [:new, :create]
+  root :to => 'pages#home'
+  resources :users, :only => [:new, :create, :index]
+  resources :mixtapes, :only => [:new, :create]
+
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
 end
