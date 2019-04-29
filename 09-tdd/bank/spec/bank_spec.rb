65dc3a97414a5097d88d9ecc1fec3e3628de31aa
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-29 10:49:57 +1000

    creates an account for a particular user

--- 09-tdd/bank/spec/bank_spec.rb
+++ 09-tdd/bank/spec/bank_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../bank'
 
 describe Bank do
+
   describe '.new' do
     it 'create a new bank object' do
       bank = Bank.new 'TDD Bank'
@@ -12,4 +13,13 @@ describe Bank do
       expect(bank.name).to eq 'TDD Bank'
     end
   end
+
+  describe '#create_account' do
+    it 'creates an account for a particular user' do
+      bank = Bank.new 'TDD Bank'
+      bank.create_account 'Craigsy', 7
+      expect(bank.accounts['Craigsy']).to eq 7
+    end
+  end
+
 end
