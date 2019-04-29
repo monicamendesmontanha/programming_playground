8a39ad5ce1348d8277bd80d151fda5a23efb6db9
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-29 14:27:13 +1000

    Add new <p>with number fact when button is clicked

--- 10-ajax/ajax-intro/js/ajax-intro.js
+++ 10-ajax/ajax-intro/js/ajax-intro.js
@@ -1,32 +1,20 @@
 const fetchFact = function () {
-  console.log('fetching fact')
+  const xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== 4) {
+      return;
+    }
+
+    const p = document.createElement('p');
+    const data = JSON.parse( xhr.responseText ); //Convert the JSON string into a JS object.
+    p.innerHTML = data.text; // data["text"]
+    document.body.appendChild(p);
+  };
+
+  xhr.open('GET', 'http://numbersapi.com/random/trivia?json');
+  xhr.send();   // Asyncronous
+
 };
 
 document.getElementById('fetch').addEventListener('click', fetchFact);
-
-// get a XHR instance
-const xhr = new XMLHttpRequest();
-
-
-// callback that fires every time the "readyState" changes.
-xhr.onreadystatechange = function () {
-  // Ignores all the readyStates except 4. Get out of this function ASAP.
-  if (xhr.readyState !== 4) {
-    return;
-  }
-  // document.body.innerHTML = '<p>' + xhr.responseText + '</p>';
-};
-
-
-// open the conection
-xhr.open('GET', 'http://numbersapi.com/random/trivia');
-
-
-// make the actual request
-xhr.send();   // Asyncronous
-
-
-// display the result
-setTimeout(function () {
-  console.log( 'The response is', xhr.responseText );
-}, 500);
