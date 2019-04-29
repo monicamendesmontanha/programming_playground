2741a118a706f644379ffe07afddb7ad8bfd809e
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-29 12:49:25 +1000

    open the conection,make request,display the result

--- 10-ajax/ajax-intro/js/ajax-intro.js
+++ 10-ajax/ajax-intro/js/ajax-intro.js
@@ -1,1 +1,15 @@
-console.log('hey')
+console.log('loading...')
+
+// get a XHR instance
+const xhr = new XMLHttpRequest();
+
+// open the conection
+xhr.open('GET', 'http://numbersapi.com/random/trivia');
+
+// make the actual request
+xhr.send();   // Asyncronous
+
+// display the result
+setTimeout(function () {
+  console.log( 'The response is', xhr.responseText );
+}, 500);
