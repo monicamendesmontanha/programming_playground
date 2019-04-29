63eadce623b8d43e3f5e83c3c026a4893eacc1fd
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-29 12:56:29 +1000

    Ignores all the readyStates except 4

--- 10-ajax/ajax-intro/js/ajax-intro.js
+++ 10-ajax/ajax-intro/js/ajax-intro.js
@@ -3,17 +3,24 @@ console.log('loading...')
 // get a XHR instance
 const xhr = new XMLHttpRequest();
 
+
 // callback that fires every time the "readyState" changes.
 xhr.onreadystatechange = function () {
-  console.log('this runs a few times', xhr.readyState, xhr.responseText);
+  // Ignores all the readyStates except 4. Get out of this function ASAP.
+  if (xhr.readyState !== 4) {
+    return;
+  }
+    console.log( 'here is the response:', xhr.responseText );
 };
 
 
 // open the conection
 xhr.open('GET', 'http://numbersapi.com/random/trivia');
 
+
 // make the actual request
 xhr.send();   // Asyncronous
+
 
 // display the result
 setTimeout(function () {
