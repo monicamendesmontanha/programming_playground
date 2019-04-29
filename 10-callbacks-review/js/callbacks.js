69f8aa26e092e2354f426cbf555073c1cb7f65d6
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-29 11:51:09 +1000

    Callbacks review

---
+++ 10-callbacks-review/js/callbacks.js
@@ -0,0 +1,8 @@
+// Callbacks Review ///////////
+
+// Timers
+console.log('A: before SetTimeout')
+setTimeout(function () {
+  console.log('B: times has passed....');
+}, 4000);
+console.log('C: after SetTimeout');
