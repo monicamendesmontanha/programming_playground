24e24863fa0a90ef9e6cac67d3f030c196b7e7e2
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-29 10:32:22 +1000

    Create a test to check when is add a new bank

--- 09-tdd/bank/spec/bank_spec.rb
+++ 09-tdd/bank/spec/bank_spec.rb
@@ -1,4 +1,10 @@
 require_relative '../bank'
 
 describe Bank do
+  describe '.new' do
+    it 'create a new bank object' do
+      bank = Bank.new
+      expect(bank).to_not be nil
+    end
+  end
 end
