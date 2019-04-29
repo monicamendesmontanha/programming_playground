f59b1ca6e3ccca15e192734441cc58c8a8d5238c
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-29 10:36:07 +1000

    Add a bank name

--- 09-tdd/bank/bank.rb
+++ 09-tdd/bank/bank.rb
@@ -1,2 +1,11 @@
 class Bank
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
 end
--- 09-tdd/bank/spec/bank_spec.rb
+++ 09-tdd/bank/spec/bank_spec.rb
@@ -3,8 +3,13 @@ require_relative '../bank'
 describe Bank do
   describe '.new' do
     it 'create a new bank object' do
-      bank = Bank.new
+      bank = Bank.new 'TDD Bank'
       expect(bank).to_not be nil
+    end
+
+    it 'assigns a bank name' do
+      bank = Bank.new 'TDD Bank'
+      expect(bank.name).to eq 'TDD Bank'
     end
   end
 end
