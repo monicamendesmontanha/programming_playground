5681f855e79b3c9caeb9f912cf16d4f79f177ba5
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-29 12:02:08 +1000

    jQuery: Document ready vs Event handlers

--- 10-callbacks-review/js/callbacks.js
+++ 10-callbacks-review/js/callbacks.js
@@ -1,8 +1,38 @@
-// Callbacks Review ///////////
+// Callbacks Review ////////////////////////////////////////////////
+// A callback is a function passed into another function which calls
+//that first function for you, at the some later point in time.
 
-// Timers
-console.log('A: before SetTimeout')
+
+//////////////////////////////////////////
+// Timers: order will  A, C, B //////////
+//////////////////////////////////////////
+console.log('A: before SetTimeout');
+
 setTimeout(function () {
   console.log('B: times has passed....');
 }, 4000);
+
 console.log('C: after SetTimeout');
+
+
+
+//////////////////////////////////////////
+// jQuery: Document ready: I, III, II ///
+//////////////////////////////////////////
+console.log('I: before document ready');
+$(document).ready(function () {
+  console.log('II: the document is now ready');
+});
+console.log('III: after document ready');
+
+
+//////////////////////////////////////////
+// Event handlers  ///////////////////////
+//////////////////////////////////////////
+console.log('Groucho');
+$(document).on('click', function () {
+  console.log('Harpo');
+});
+console.log('Chico');
+
+
