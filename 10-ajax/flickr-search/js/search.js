a63587f4229d792f4cfc712861d0a29765ebec26
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-01 10:44:12 +1000

    AJAX Flickr Lab

--- 10-ajax/flickr-search/js/search.js
+++ 10-ajax/flickr-search/js/search.js
@@ -1,3 +1,12 @@
+// Global variables
+const state = {
+  currentPage: 1,            //Support infinite scroll to show all results from Flickr (eventually, after a lot of scrolling).
+  lastPageReached: false,    // Don't request more images when you reach the last "page" of Flickr results
+  requestInProgress: false   //Prevent your code from making too many requests: only one at a time, please
+};
+
+// Prevent from making too many requests: only one at a time, please
+
 const showImages = function (results) {
 
   // Nested AKA helper function that returns a thumbnail URL for a given photo object.
@@ -22,20 +31,30 @@ const showImages = function (results) {
   });
 };
 
-let currentPage = 1;
+
 
 const searchFlickr = function (terms) {
+
+  if (state.lastPageReached || state.requestInProgress) return; // Don't request more images when you reach the last "page" of Flickr results // Prevent your code from making too many requests: only one at a time, please
+
   console.log(`Searching Flickr for ${ terms }`);
 
   const flickrURL = 'https://api.flickr.com/services/rest?jsoncallback=?';
+
+  state.requestInProgress = true;
+
   $.getJSON(flickrURL, {
     method: 'flickr.photos.search',
     api_key: '2f5ac274ecfac5a455f38745704ad084', // not a secret key
     text: terms,
     format: 'json',
-    page: currentPage++
+    page: state.currentPage++ //Support infinite scroll to show all results from Flickr (eventually, after a lot of scrolling).
   }).done( showImages ).done(function (data) {
     console.log( data );
+    if (data.photos.page > data.photos.pages) {
+      state.lastPageReached = true;  // Don't request more images when you reach the last "page" of Flickr results
+    }
+    state.requestInProgress = false; // Prevent your code from making too many requests: only one at a time, please
   });
 };
 
@@ -45,6 +64,8 @@ $(document).ready(function () {
 
     const query = $('#query').val();
     $('#images').empty();
+    state.lastPageReached = false;
+    state.currentPage = 1;
     searchFlickr(query);
   });
 
