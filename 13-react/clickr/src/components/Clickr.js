8600c490018062a5b29a617b0f9080a92e588d9f
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-03 12:41:50 +1000

    Add the setState to change the value of state

--- 13-react/clickr/src/components/Clickr.js
+++ 13-react/clickr/src/components/Clickr.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 
 class Clickr extends Component {
-  _incrementClicks() {
-    console.log('yes, a click happened. there was a click')
+  constructor() { //This runs the original React Component constructor which React needs to set up.
+    super();
+    this.state = { clicks: 0 } // Set up out initial state.
+    this._incrementClicks = this._incrementClicks.bind(this); // Every event handle that needs acess to 'this' should be bound in the constructor.
   }
+
+
+  _incrementClicks() {
+    console.log( this );
+    // this.state.clicks += 1; // Mutation won't work
+    this.setState({clicks: this.state.clicks +1});
+  }
+
 
   render() {
     return (
-      <button  onClick={ this._incrementClicks }>0 clicks so far</button>
+      <button  onClick={ this._incrementClicks }>{ this.state.clicks } clicks so far</button>
     )
   }
 };
