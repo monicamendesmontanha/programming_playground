b52588982cf384450fe297f9eb6fa7717fdce39b
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-03 13:00:50 +1000

    Add component Profile to show at initial page

--- 13-react/social-network/src/components/App.js
+++ 13-react/social-network/src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Profile from './Profile';
 
 function App() {
   return (
     <div className="App">
-      <h1>Profile coming soon</h1>
+      <Profile name="Groucho" age="101" motto="No gods no master" pic="https://fillmurray.com/200/300"/>
+      <Profile name="Harpo" age="98" motto="No gods no master" pic="https://fillmurray.com/401/400"/>
+      <Profile name="Chico" age="99" motto="No gods no master" pic="https://fillmurray.com/400/400"/>
     </div>
   );
 }
