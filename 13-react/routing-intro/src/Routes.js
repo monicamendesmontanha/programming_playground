b8e93b43b259de0b1441558c4df04ac7ab0e87fc
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-06 14:50:36 +1000

    Change from HashRouter to BrowserRouter

--- 13-react/routing-intro/src/Routes.js
+++ 13-react/routing-intro/src/Routes.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+// import { HashRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Home from './components/Home';
 import FAQ from './components/FAQ';
 
 const Routes = (
-  <HashRouter>
+    <BrowserRouter>
     <div>
       <Route exact path="/" component={ Home } />
       <Route exact path="/faq" component={ FAQ } />
     </div>
-  </HashRouter>
+  </BrowserRouter>
 )
 
 export default Routes;
