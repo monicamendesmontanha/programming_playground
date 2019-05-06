49dc468f283ef4166b7218c60825fe0998956ec7
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-06 15:47:26 +1000

    Initial setup project react ajax api stock

---
+++ 13-react/stock/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Stock from './Stock';
+
+function App() {
+  return (
+    <div className="App">
+     <Stock />
+    </div>
+  );
+}
+
+export default App;
---
+++ 13-react/stock/src/components/Stock.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Stock extends Component {
+  render() {
+    return (
+      <div>
+        <h1>STOCK</h1>
+      </div>
+
+    );
+  }
+}
+
+export default Stock;
