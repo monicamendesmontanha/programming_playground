aa918c61a71ad9b869a3650682b9cac6e55727e7
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-06 14:48:13 +1000

    Add link at faq page to go to home page

--- 13-react/routing-intro/src/components/FAQ.js
+++ 13-react/routing-intro/src/components/FAQ.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const FAQ = () => {
   return (
     <div>
       <h2>Frequently Asked Questions</h2>
+      <p>
+        Coming soon! Go back <Link to="/">home</Link>.
+      </p>
     </div>
   )
 };
