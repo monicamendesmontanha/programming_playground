4ec79afb84a1d0bf8e2c22c10284e23cb7e71bab
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-07 11:54:53 +1000

    Initial Setup project secrets client

---
+++ 13-react/secrets_client/src/components/Secrets.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Secrets extends Component {
+  render() {
+    return(
+      <div>
+        coming soon
+      </div>
+
+    );
+  }
+}
+
+export default Secrets;
