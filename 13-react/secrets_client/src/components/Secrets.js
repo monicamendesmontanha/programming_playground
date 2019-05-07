a3b05fb0820da05442454da1ebf9f91a3c7d7067
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-07 11:56:51 +1000

    Add secret form and gallery components

--- 13-react/secrets_client/src/components/Secrets.js
+++ 13-react/secrets_client/src/components/Secrets.js
@@ -4,11 +4,29 @@ class Secrets extends Component {
   render() {
     return(
       <div>
-        coming soon
+        <h1>Tell us all your secrets</h1>
+        <SecretForm />
+        <Gallery />
       </div>
 
     );
   }
-}
+};
+
+class SecretForm extends Component {
+  render() {
+    return(
+      <h2>Form</h2>
+    );
+  }
+};
+
+class Gallery extends Component {
+  render() {
+    return(
+      <h2>Gallery</h2>
+    );
+  }
+};
 
 export default Secrets;
