d22bb64f32ddfc12a9f3c6b96d9c7a5b9a761dca
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-21 12:48:11 +1000

    Refactor the Port

--- 19-express/express-intro/index.js
+++ 19-express/express-intro/index.js
@@ -3,10 +3,12 @@ const express = require('express');
 // call express
 const server = express();
 
+const PORT = 1337
+
 // whats going on the server
 server.get('/', (req, res) => {
   console.log('There was request');
   res.send('How ya doing');
 });
 
-server.listen(3000, () => console.log('Now serving on htttp://localhost:3000/'));
+server.listen(PORT, () => console.log(`Now serving on htttp://localhost:${PORT}/`));
