5048b2484cd7ad468d77acd6426083937ab2c2bb
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-21 12:56:33 +1000

    Add a home.ejs and render a new page from server

--- 19-express/express-intro/index.js
+++ 19-express/express-intro/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 
+// npm install ejs
+const ejs = require('ejs')
+
 // call express
 const server = express();
+server.set('view-engine', 'ejs');   // See documentation.
 
 const PORT = 1337
 
 // whats going on the server
 server.get('/', (req, res) => {
-  console.log('There was request');
-  res.send('How ya doing');
+  console.log('GET /');
+  res.render('home.ejs');
 });
 
 server.get('/about', (req, res) => {
