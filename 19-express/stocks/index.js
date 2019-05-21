8ba4fee0ed19fedd48e4487402a80a0615c61374
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-21 15:06:47 +1000

    create express.js app that fetches data from API.

---
+++ 19-express/stocks/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const ejs = require('ejs');
+const IEX = require('iex-api');
+const _fetch = require('isomorphic-fetch');
+
+const iex = new IEX.IEXClient(_fetch);
+
+
+const PORT = 1337;
+
+const server = express();
+server.set('view-engine', 'ejs');
+
+server.get('/', (req, res) => {
+  res.render('home.ejs');
+});
+
+server.get('/info', (req, res) => {
+  const symbol = req.query.symbol;
+  // get the actual stock price
+  iex.stockQuote(symbol).then((quote) => {
+    res.render('info.ejs', {symbol, quote}); // {symbol: symbol, quote: quote}
+    // console.log(quote);
+  })
+  // render the result view
+});
+
+server.listen(PORT, () => console.log(`Listening on http://localhost:${ PORT }`))
