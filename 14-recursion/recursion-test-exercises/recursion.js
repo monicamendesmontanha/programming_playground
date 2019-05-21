057aa29f1438bc42303c5576e5a9707c579866c0
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-21 11:01:30 +1000

    Add coinFlips recursion

--- 14-recursion/recursion-test-exercises/recursion.js
+++ 14-recursion/recursion-test-exercises/recursion.js
@@ -14,10 +14,14 @@ function findMax (array, max=-Infinity) {
     return findMax(array.slice(1), max);
 }
 
+
+
 function factorial (n) {
   // This function returns the factorial of a given number.
   return (n <= 1) ? 1 : n * factorial(n - 1);
 }
+
+
 
 // This function returns the Nth number in the fibonacci sequence.
 // https://en.wikipedia.org/wiki/Fibonacci_number
@@ -27,14 +31,36 @@ function fibonacci (n, a=1, b=1) {
   return (n <= 2) ? b : fibonacci(n-1, b, a+b);
 }
 
-function coinFlips(){
-    // This function returns an array of all possible outcomes from flipping a coin N times.
-    // Input type: Integer
-    // For example, coinFlips(2) would return the following:
-    // ["HH", "HT", "TH", "TT"]
-    // H stands for Heads and T stands for tails
-    // Represent the two outcomes of each flip as "H" or "T"
+
+
+
+// This function returns an array of all possible outcomes from flipping a coin N times.
+// Input type: Integer
+// For example, coinFlips(2) would return the following:
+// ["HH", "HT", "TH", "TT"]
+// H stands for Heads and T stands for tails
+// Represent the two outcomes of each flip as "H" or "T"
+function coinFlips (tosses){
+  const combinations = [];
+
+  // define a recursive function
+  const flip = function (soFar="") {
+    if (soFar.length === tosses) {
+      combinations.push(soFar); // base case
+    } else {
+      // recursive
+      flip(soFar + 'H');
+      flip(soFar + 'T');
+    }
+  };
+
+  flip();
+
+  return combinations;
 }
+
+
+
 
 function letterCombinations(){
     // This function returns an array of all combinations of the given letters
