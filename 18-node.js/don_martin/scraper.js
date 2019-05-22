c118c977cfbe6a86c50b949d4686f298ca0be2e8
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-22 10:30:32 +1000

    fetch the URL

--- 18-node.js/don_martin/scraper.js
+++ 18-node.js/don_martin/scraper.js
@@ -4,6 +4,13 @@ const sourceURL = 'https://www.madcoversite.com/dmd-alphabetical.html';
 
 
 //  1. fetch the URL
+axios( sourceURL ).then((response) => {
+  console.log(response.data );
+});
+
+
 //  2. interate through the results
 //  3. push each sound into an array
 //  4. save the array to a file
+
+
