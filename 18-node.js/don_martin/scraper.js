07731e96c70fe2086d40babbe65644e5f3332a7d
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-22 10:27:15 +1000

    Setup  node scraping

---
+++ 18-node.js/don_martin/scraper.js
@@ -0,0 +1,9 @@
+const axios = require('axios');
+
+const sourceURL = 'https://www.madcoversite.com/dmd-alphabetical.html';
+
+
+//  1. fetch the URL
+//  2. interate through the results
+//  3. push each sound into an array
+//  4. save the array to a file
