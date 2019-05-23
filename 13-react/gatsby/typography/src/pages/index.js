4883f6c4e2c0db32db64f823ecbe517d16cf6877
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-23 14:40:08 +1000

    import the same layout to all pages

--- 13-react/gatsby/typography/src/pages/index.js
+++ 13-react/gatsby/typography/src/pages/index.js
@@ -1,8 +1,9 @@
 import React from "react"
+import Layout from "../components/layout"
 
 export default () => (
-  <div style={ {color: 'green', margin: '3em auto', maxWidth: 600, textAlign: 'justify'} }>
+  <Layout>
     <h1>Gates Typography</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sapiente ipsa ab harum fugiat repellat expedita rem cupiditate libero quidem voluptatibus, quod esse quaerat aliquid laborum optio explicabo quasi deserunt.</p>
-  </div>
+  </Layout>
 );
