ba9ab812bda2e9b6cc10bd4e1dfd50a12f1fd430
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-23 14:50:24 +1000

    Create navigation between the pages

--- 13-react/gatsby/typography/src/components/layout.js
+++ 13-react/gatsby/typography/src/components/layout.js
@@ -1,8 +1,25 @@
-import React from "react"
+import React from "react";
+import { Link } from "gatsby";
+
+const NavLink = (props) => (  // DECONSTRUCTION: const NavLink = (to, children ) => (
+  <li style={{display: 'inline-block', marginRight: '1em'}}>
+    <Link to={props.to}>{props.children}</Link>
+  </li>
+);
+
 
 export default ({children}) => (
   <div style={ {color: 'green', margin: '3em auto', maxWidth: 650, padding: '0 1em', textAlign: 'justify'} }>
-    <h3>My Site</h3>
+    <header style={{marginBottom: '1.5em'}}>
+      <Link to="/">
+        <h3 style={{display: 'inline'}}>My Site</h3>
+      </Link>
+    </header>
+
+    <ul style={{display: 'inline'}}>
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/contact">Contact</NavLink>
+    </ul>
     {children}
   </div>
 );
