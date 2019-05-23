5f5f060e68b5945500bfd0d4583b5bc231e57473
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-23 14:41:40 +1000

    Add a element to show in every page (h3)

--- 13-react/gatsby/typography/src/components/layout.js
+++ 13-react/gatsby/typography/src/components/layout.js
@@ -2,6 +2,7 @@ import React from "react"
 
 export default ({children}) => (
   <div style={ {color: 'green', margin: '3em auto', maxWidth: 650, padding: '0 1em', textAlign: 'justify'} }>
+    <h3>My Site</h3>
     {children}
   </div>
 );
