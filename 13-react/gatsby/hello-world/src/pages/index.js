21d34fade1e9cfa7eed46c879e0d8e026f82010a
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-23 12:00:05 +1000

    Add link between pages

--- 13-react/gatsby/hello-world/src/pages/index.js
+++ 13-react/gatsby/hello-world/src/pages/index.js
@@ -1,9 +1,12 @@
-import React from "react"
+import React from "react";
+import { Link } from "gatsby";
+import Header from '../components/header';
 
 export default () => (
 
   <div style={ { color: 'purple'} }>
-    <h1>Hey World</h1>
+    <Header headerText="Hello World" />
+    <Link to="/about">About me</Link>
     <p>Oh yes</p>
     <img src="https://source.unsplash.com/random/400x200" alt="Random image" />
   </div>
