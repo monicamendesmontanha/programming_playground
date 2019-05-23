aeaa26ca9f43d1ff62cb34780fffe1b758bb2438
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-23 11:46:46 +1000

    Add random image

--- 13-react/gatsby/hello-world/src/pages/index.js
+++ 13-react/gatsby/hello-world/src/pages/index.js
@@ -5,5 +5,6 @@ export default () => (
   <div style={ { color: 'purple'} }>
     <h1>Hey World</h1>
     <p>Oh yes</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="Random image" />
   </div>
 );
