ea1457b324bb3ceeb79fcf7cae86b42877af96f5
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-23 11:20:11 +1000

    Bubble sort solution

--- 0x-bonus/basic-sorts/BubbleSort.js
+++ 0x-bonus/basic-sorts/BubbleSort.js
@@ -17,19 +17,22 @@
 
 function bubbleSort(array) {
 
-  let swapped = true;
+  let swapped = true; // Assume the worst
+  // We don't need to consider elements that have bubbled to the end.
+  let end = array.length;
 
-  while(swapped === true) {
+  while (swapped === true) {
     swapped = false;
-    for (let i = 0; i > array.length - 1; i++) {
+    for (let i = 0; i < end; i++) {
       if (array[i] > array[i + 1]) {
-        const temp = array[i + 1];
-        array[i + 1] = array[i];
-        array[i] = temp;
+
+        // ES6ier with parallel assignment/destructuring:
+        [ array[i], array[i+1] ] = [ array[i+1], array[i] ];
 
         swapped = true;
       }
     }
+    end--;
   }
 
   return array;
