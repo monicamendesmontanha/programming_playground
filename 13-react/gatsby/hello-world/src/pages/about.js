6acccd91ce5b9237f9ab02983b5a14019925ba07
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-23 11:49:27 +1000

    Add about page

---
+++ 13-react/gatsby/hello-world/src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default () => (
+  <div style={ { color: 'teal'} }>
+    <h1>About Gatsby</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat minima praesentium illo numquam et accusamus itaque ullam unde facilis sint fuga eligendi sequi optio delectus, asperiores doloremque ipsam, repellat nesciunt.</p>
+
+  </div>
+
+);
