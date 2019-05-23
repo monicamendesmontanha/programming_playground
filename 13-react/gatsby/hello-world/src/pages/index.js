27a68cf08dbf5bb32b730e84c23056b5f4c1c138
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-23 12:04:09 +1000

    Add contact page and the link to go to contact pag

--- 13-react/gatsby/hello-world/src/pages/index.js
+++ 13-react/gatsby/hello-world/src/pages/index.js
@@ -7,6 +7,7 @@ export default () => (
   <div style={ { color: 'purple'} }>
     <Header headerText="Hello World" />
     <Link to="/about">About me</Link>
+    <Link to="/contact">Contact us</Link>
     <p>Oh yes</p>
     <img src="https://source.unsplash.com/random/400x200" alt="Random image" />
   </div>
