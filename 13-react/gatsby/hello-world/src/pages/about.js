7ef1440581a38eea497b8137d2625e725bbac0ba
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-23 12:11:09 +1000

    Add link from about to home

--- 13-react/gatsby/hello-world/src/pages/about.js
+++ 13-react/gatsby/hello-world/src/pages/about.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { Link } from "gatsby";
 import Header from '../components/header';
 
 export default () => (
   <div style={ { color: 'teal'} }>
+    <Link to="/">Home</Link>
     <Header headerText="About me" />
     <Header headerText="Gatsby" />
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat minima praesentium illo numquam et accusamus itaque ullam unde facilis sint fuga eligendi sequi optio delectus, asperiores doloremque ipsam, repellat nesciunt.</p>
