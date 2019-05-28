5ef94c6ed2f4bd75ccd34ba09affc912a5bf2a38
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-28 10:50:17 +1000

    promises

--- 01-javascript/promises/promises.js
+++ 01-javascript/promises/promises.js
@@ -4,15 +4,48 @@
 
 const fs = require('fs');
 
-// readFile is asynchronous
+// // readFile is asynchronous
 
-fs.readFile('flinststones.txt', 'utf-8', (err, content) => {
-  // error first pattern
-  if (err) {
-    throw err;
-  }
+// fs.readFile('flinststones.txt', 'utf-8', (err, content) => {
+//   // error first pattern
+//   if (err) {
+//     throw err;
+//   }
 
-  console.log( content );
+//   console.log( content );
+// });
+
+
+// A promise wrapper around readFile
+
+const readFile = (filename) => {
+  return new Promise( (resolve, reject) => {
+    fs.readFile(filename, 'utf-8', (err, content) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve(content);
+    });
+  });
+};
+
+// readFile('flinststones.txt').then((content) => {
+//   console.log('This .then() callback eas actually invoked under the name resolve()');
+//   console.log('content');
+// }).catch((error) => {
+//   console.log('There was error', error);
+// });
+
+const fileReadingPromise = readFile('flinstones.txt');
+fileReadingPromise.then( (content) => {
+  console.log(content);
 });
 
+console.log('other stuff');
+
+fileReadingPromise.catch( (err) => {
+  console.log('There was error', err);
+});
 
