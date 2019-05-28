1ad5302202d0d3aa18676395604c9456661205e4
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-28 11:23:03 +1000

    A new solution

--- 01-javascript/promises/helloWorld.js
+++ 01-javascript/promises/helloWorld.js
@@ -1,5 +1,14 @@
+// async function test() {
+//   await new Promise((resolve, reject) => setTimeout(() => resolve(), 1000));
+//   console.log('Hello, World!');
+// }
+
+// test();
+
 async function test() {
-  await new Promise((resolve, reject) => setTimeout(() => resolve(), 1000));
+  // This object is a "thenable". It's a promise by the letter of the law,
+  // but not the spirit of the law.
+  await { then: resolve => setTimeout(() => resolve(), 1000) };
   console.log('Hello, World!');
 }
 
