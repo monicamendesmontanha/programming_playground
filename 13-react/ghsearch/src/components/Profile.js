b3ea5606a614a4429c11c6c4e264a4da750308d1
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-29 12:53:38 +1000

    Create a Search component

--- 13-react/ghsearch/src/components/Profile.js
+++ 13-react/ghsearch/src/components/Profile.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default (props) => {
-
-  return (
-      <h1> Profile </h1>
-  );
+export default class Profile extends Component {
+  render() {
+    return (
+      <h2>{ this.props.match.params.username }'s Profile</h2>
+    );
+  }
 };
