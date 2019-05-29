ab68e1c7361c9e73b4e8aa5d328a9c47ac65083d
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-29 14:12:35 +1000

    Find a random user and push that into the history

--- 13-react/ghsearch/src/components/Home.js
+++ 13-react/ghsearch/src/components/Home.js
@@ -3,12 +3,45 @@ import { Link } from 'react-router-dom';
 
 export default (props) => {
 
+  const _randomUser = () => {
+    const students = [
+      'monicamendesmontanha',
+      'Trigotometry',
+      'nivvyart',
+      'mmborres',
+      'patricianunes',
+      'edgarjin88',
+      'LukeAnton',
+      'pandyapurvi',
+      'AnthonyGDoueihi',
+      'jamaspy',
+      'ritikagoel87',
+      'rashidabengali',
+      'milasv',
+      'ChrisLaneAU',
+      'SamSeabourn',
+      'Michaelfov',
+      'hluscombe',
+      'FireMugen',
+      'adlanelias',
+      'bennnym'
+     ];
+
+     const index = Math.floor(Math.random() * students.length );
+     // find a random user
+     const username = students[ index ];
+     console.log( username );
+     // push that into the history
+     props.history.push(`/details/${ username }`);
+  };
+
   return (
     <div className="Home">
       <h1> Github Search </h1>
       <Link to="/search">
       <button>Search for a user</button>
       </Link>
+      <button onClick={ _randomUser }>Random user</button>
 
     </div>
   );
