b7a79fdef2f84c2b0953e24fc940ed7ab221ff72
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-03 11:03:17 +1000

    Add reducer

--- 13-react/redux-counter/src/App.js
+++ 13-react/redux-counter/src/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import Counter from './components/Counter';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const initialState = {
+  count: 0
+};
+
+// Set of rules to take the current state and an action and return a new state
+const reducer = (state=initialState, action) => {
+  switch(action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    default:
+    return state;   // The reduce must always return a state object.
+  }
+};
+
+const store = createStore( reducer );
+
 function App() {
   return (
     <div className="App">
-    <Counter />
+      <Provider store={store}>
+        <Counter />
+      </Provider>
     </div>
   );
 }
