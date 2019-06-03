ea506b68bd21c4b93aa070a89555b97deacfad57
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-03 12:40:57 +1000

    Export the query (graphql)

--- 20-graphql/graphql/src/pages/about.js
+++ 20-graphql/graphql/src/pages/about.js
@@ -1,11 +1,22 @@
-import React from "react"
+import React from "react";
+import { graphql } from "gatsby";
 import Layout from "../components/layout";
 
-export default () => (
+export default (props) => (
   <Layout>
-    <h1>About Bill Murray</h1>
+    <h1>{props.data.site.siteMetadata.title}</h1>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati repudiandae commodi tenetur quo, distinctio exercitationem facilis quibusdam. Assumenda, iure eveniet similique voluptates veniam nemo obcaecati nisi, quasi quas officiis in!
     </p>
   </Layout>
-)
+);
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
