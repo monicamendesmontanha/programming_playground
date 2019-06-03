a7694c967c85d63ff591e096ebea64ba3b5fefde
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-03 12:28:24 +1000

    Add about page

---
+++ 20-graphql/graphql/src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout";
+
+export default () => (
+  <Layout>
+    <h1>About Bill Murray</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati repudiandae commodi tenetur quo, distinctio exercitationem facilis quibusdam. Assumenda, iure eveniet similique voluptates veniam nemo obcaecati nisi, quasi quas officiis in!
+    </p>
+  </Layout>
+)
