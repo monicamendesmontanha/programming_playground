61a7d2751cff719d1a98193411cfe99af856ff3c
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-03 10:29:02 +1000

    Fix counter

--- 13-react/redux-counter/src/components/Counter.js
+++ 13-react/redux-counter/src/components/Counter.js
@@ -3,17 +3,17 @@ import React, { Component } from 'react';
 class Counter extends Component {
   constructor() {
     super();
-    this.state = { Counter: 0 };
+    this.state = {count: 0};
     this._increment = this._increment.bind(this);
     this._decrement = this._decrement.bind(this);
   }
 
   _increment() {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({count: this.state.count + 1});
   }
 
   _decrement() {
-    this.setState({ count: this.state.count - 1 })
+    this.setState({count: this.state.count - 1})
   }
 
   render() {
@@ -22,11 +22,11 @@ class Counter extends Component {
         <h2>Counter</h2>
         <div>
           <button onClick={this._decrement}>-</button>
-          <span>{ this.state.count }</span>
+          <span>{this.state.count}</span>
           <button onClick={this._increment}>+</button>
         </div>
       </div>
-    )
+    );
   }
 }
 
