628067fa90933b8aaf4c77f5eccb0cef57bc83f0
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-03 12:46:00 +1000

    Add useStaticQuery and graphql

--- 20-graphql/graphql/src/components/layout.js
+++ 20-graphql/graphql/src/components/layout.js
@@ -1,38 +1,52 @@
 import React from 'react';
 import { css } from '@emotion/core';
-import { Link } from 'gatsby';
-
+import { useStaticQuery, Link, graphql } from 'gatsby';
 import { rhythm } from '../utils/typography';
 
 
-export default ({children}) => (
-  <div css={css`
-    margin: 0 auto;
-    max-width: 700px;
-    padding: ${rhythm(2)};
-    padding-top: ${rhythm(1.5)};
-    `}
-    >
+export default ({children}) => {
 
-    <Link to={'/'}>
-      <h3
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  );
+
+  return (
+    <div css={css`
+      margin: 0 auto;
+      max-width: 700px;
+      padding: ${rhythm(2)};
+      padding-top: ${rhythm(1.5)};
+      `}
+      >
+
+      <Link to={'/'}>
+        <h3
+          css={css`
+            margin-bottom: ${rhythm(2)};
+            display: inline-block;
+            font-style: normal;
+            `}
+        >
+          {data.site.siteMetadata.title}
+        </h3>
+      </Link>
+      <Link to={'/about'}
         css={css`
-          margin-bottom: ${rhythm(2)};
-          display: inline-block;
-          font-style: normal;
+          float: right;
           `}
       >
-        Bill Murral Fan Site
-      </h3>
-    </Link>
-    <Link to={'/about'}
-      css={css`
-        float: right;
-        `}
-    >
-      About
-    </Link>
-    {children}
+        About
+      </Link>
+      {children}
 
-  </div>
-)
+    </div>
+    );
+};
